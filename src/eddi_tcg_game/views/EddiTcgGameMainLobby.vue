ef325a755f51649ea58e785d236d3ff7e271e8b2
silenc3502 <gcccompil3r@gmail.com>
2024-03-29 14:13:38 +0900

    init: tailwind env

--- src/eddi_tcg_game/views/EddiTcgGameMainLobby.vue
+++ src/eddi_tcg_game/views/EddiTcgGameMainLobby.vue
@@ -1,21 +1,41 @@
 <template>
-    <div ref="rendererContainer"></div>
+    <div class="container">
+        <!-- 캔버스 컨테이너 -->
+        <div ref="rendererContainer" class="canvas-container"></div>
+
+        <!-- 배경 이미지 -->
+        <div class="background-image"></div>
+
+        <!-- 추가 이미지 -->
+        <div class="image-grid">
+            <button class="image-button" v-for="(image, index) in images" :key="index" @click="handleButtonClick(image)">
+                <img :src="image.src" alt="image">
+            </button>
+        </div>
+    </div>
 </template>
 
 <script>
 import * as THREE from 'three';
+import mainLobbyBackgroundImage from '@/assets/eddi_tcg_game/mainLobbyBackgroundImage.png';
+import entranceBattleFieldButton from '@/assets/eddi_tcg_game/entranceBattleFieldButton.png';
 
 export default {
+    data() {
+        return {
+            images: [
+                { src: entranceBattleFieldButton },
+            ]
+        };
+    },
     mounted() {
-        // 캔버스 엘리먼트 생성
+        const navbarHeight = document.querySelector('.v-app-bar').clientHeight;
+
         this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setSize(window.innerWidth, window.innerHeight - navbarHeight);
         this.$refs.rendererContainer.appendChild(this.renderer.domElement);
 
-        // 씬 생성
         this.scene = new THREE.Scene();
-
-        // 카메라 생성
         this.camera = new THREE.PerspectiveCamera(
             75,
             window.innerWidth / window.innerHeight,
@@ -24,34 +44,87 @@ export default {
         );
         this.camera.position.z = 5;
 
-        // 박스 생성
+        const textureLoader = new THREE.TextureLoader();
+        const backgroundTexture = textureLoader.load(mainLobbyBackgroundImage);
+        this.scene.background = backgroundTexture;
+
         const geometry = new THREE.BoxGeometry();
         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         this.cube = new THREE.Mesh(geometry, material);
         this.scene.add(this.cube);
 
-        // 애니메이션 함수 호출
         this.animate();
     },
     methods: {
         animate() {
-            // 큐브 회전
             this.cube.rotation.x += 0.01;
             this.cube.rotation.y += 0.01;
 
-            // 씬과 카메라를 렌더러에 렌더링
             this.renderer.render(this.scene, this.camera);
-
-            // 다음 프레임 요청
             requestAnimationFrame(this.animate);
         }
     }
 };
 </script>
 
-<style>
+<style lang="postcss" scoped>
 #rendererContainer {
     width: 100%;
+    height: calc(100vh - 64px);
+}
+
+.container {
+    position: relative;
+}
+
+.canvas-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 0;
+}
+
+.background-image {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    width: 100%;
     height: 100%;
+    background-image: url('@/assets/eddi_tcg_game/mainLobbyBackgroundImage.png');
+    background-size: cover;
+    z-index: -1;
+}
+
+.image-grid {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    transform: translateX(-20%) translateY(200%);
+}
+
+
+.image-item {
+    margin-bottom: 20px;
+}
+
+.image-item img {
+    max-width: 100px;
+    height: auto;
+}
+
+.image-button {
+    margin-bottom: 20px;
+    background: none;
+    border: none;
+    cursor: pointer;
+}
+
+.image-button img {
+    max-width: 40%;
+    height: auto;
 }
 </style>
