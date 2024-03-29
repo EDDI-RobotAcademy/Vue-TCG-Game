a2b378200fced66a54567bf4f8b3ae775eeefc51
silenc3502 <gcccompil3r@gmail.com>
2024-03-29 23:41:59 +0900

    init: battle_field init

--- src/eddi_tcg_game_battle_field/views/EddiTcgGameBattleField.vue
+++ src/eddi_tcg_game_battle_field/views/EddiTcgGameBattleField.vue
@@ -31,14 +31,14 @@ export default {
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.setClearColor(0xcccccc, 0.0)
 
-        // 배경 이미지를 Three.js로 그리기
-        const textureLoader = new THREE.TextureLoader();
-        const backgroundTexture = textureLoader.load(battleFieldBackgoundImage);
-        const aspectRatio = window.innerWidth / window.innerHeight;
-        const backgroundGeometry = new THREE.PlaneGeometry(aspectRatio * 2, 2, 0);
-        const backgroundMaterial = new THREE.MeshBasicMaterial({map: backgroundTexture});
-        const backgroundMesh = new THREE.Mesh(backgroundGeometry, backgroundMaterial);
-        scene.add(backgroundMesh);
+        // // 배경 이미지를 Three.js로 그리기
+        // const textureLoader = new THREE.TextureLoader();
+        // const backgroundTexture = textureLoader.load(battleFieldBackgoundImage);
+        // const aspectRatio = window.innerWidth / window.innerHeight;
+        // const backgroundGeometry = new THREE.PlaneGeometry(aspectRatio * 2, 2, 0);
+        // const backgroundMaterial = new THREE.MeshBasicMaterial({map: backgroundTexture});
+        // const backgroundMesh = new THREE.Mesh(backgroundGeometry, backgroundMaterial);
+        // scene.add(backgroundMesh);
 
         // 큐브를 추가하여 Three.js의 렌더링 적용
         const cubeGeometry = new THREE.BoxGeometry();
@@ -68,9 +68,9 @@ export default {
     width: 100vw; /* 화면 너비에 맞추기 */
     height: 100vh; /* 화면 높이에 맞추기 */
     background-image: url(@/assets/eddi_tcg_game/battle_field/battleFieldBackgoundImage.png);
-    background-size: cover; /* 배경 이미지를 화면에 꽉 채우기 */
-    background-position: center; /* 배경 이미지를 중앙에 정렬하기 */
-    background-repeat: no-repeat; /* 배경 이미지 반복 제거 */
+    background-size: 100% 100%;
+    /*background-position: center; !* 배경 이미지를 중앙에 정렬하기 *!*/
+    /*background-repeat: no-repeat; !* 배경 이미지 반복 제거 *!*/
 }
 
 .canvas-container {
